8af55a5f1ad2ef541864334538e5714b06ddb369
CaptainAlpha04 <aliimran.iam59@gmail.com>
2025-04-26 16:51:07 +0500

    refactor: update themes in globals.css, clean up page component, and enhance server configuration

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='p-10'>
-      <h1>Hello World</h1>
-      <button className='btn'> Button </button>
-    </div>
-  )
+    <>
+    </>
+  ) 
 }
 
 export default page
--- server/server.mjs
+++ server/server.mjs
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 // Initializing the middleware
 app.use(cors())
